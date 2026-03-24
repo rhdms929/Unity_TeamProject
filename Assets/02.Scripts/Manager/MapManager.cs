a6ca42c409b6a56ed731be6b2276729b2c8a2b0b
rhdms929 <129916599+rhdms929@users.noreply.github.com>
2026-03-24 18:43:02 +0900

    add Skeleton Enemy and Fix Ui
    - Skeleton 몬스터 추가 (프리팹, 애니메이터 세팅)

--- Assets/02.Scripts/Manager/MapManager.cs
+++ Assets/02.Scripts/Manager/MapManager.cs
@@ -5,13 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class MapManager : MonoBehaviour
 {
-	public void StayInMap1(GameObject uiPanel)
+	public static MapManager Instance;
+
+	void Awake()
 	{
-		uiPanel.SetActive(false);
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
 	}
 
 	// Map2 버튼용 (다른 씬으로 이동)
-	public void GoToMap2(string sceneName)
+	public void GoToMap(string sceneName)
 	{
 		SceneManager.LoadScene(sceneName);
 	}
